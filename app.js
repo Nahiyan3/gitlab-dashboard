511330b2da4ebdb629c504e285084bed0caa181b
Nahiyan3 <Nahiyan49@gmail.com>
2025-10-06 15:52:36 +0600

    More Info about a project is added

--- app.js
+++ app.js
@@ -25,27 +25,62 @@ app.get('/', async (req, res) => {
 
 
 
+
 app.get('/project/:id', async (req, res) => {
     const projectId = req.params.id;
     try {
+        // Fetch project info
         const projectResponse = await axios.get(`https://gitlab.com/api/v4/projects/${projectId}`, {
             headers: { 'Private-Token': GITLAB_TOKEN }
         });
 
+        // Fetch commits
         const commitsResponse = await axios.get(`https://gitlab.com/api/v4/projects/${projectId}/repository/commits`, {
             headers: { 'Private-Token': GITLAB_TOKEN }
         });
 
+        // Fetch all issues
+        const issuesResponse = await axios.get(`https://gitlab.com/api/v4/projects/${projectId}/issues?per_page=100`, {
+            headers: { 'Private-Token': GITLAB_TOKEN }
+        });
+
+        // Fetch branches
+        const branchesResponse = await axios.get(`https://gitlab.com/api/v4/projects/${projectId}/repository/branches`, {
+            headers: { 'Private-Token': GITLAB_TOKEN }
+        });
+        // Fetch merge requests
+         const [openMRs, closedMRs, mergedMRs] = await Promise.all([
+            axios.get(`https://gitlab.com/api/v4/projects/${projectId}/merge_requests?state=opened&per_page=100`, {
+                headers: { 'Private-Token': GITLAB_TOKEN }
+            }),
+            axios.get(`https://gitlab.com/api/v4/projects/${projectId}/merge_requests?state=closed&per_page=100`, {
+                headers: { 'Private-Token': GITLAB_TOKEN }
+            }),
+            axios.get(`https://gitlab.com/api/v4/projects/${projectId}/merge_requests?state=merged&per_page=100`, {
+                headers: { 'Private-Token': GITLAB_TOKEN }
+            })
+        ]);
+
         const project = projectResponse.data;
         const commits = commitsResponse.data;
-        const totalCommits = commits.length; // Total number of commits
+        const totalCommits = commits.length;
+        const branches = branchesResponse.data;
 
-        res.render('project', { project, totalCommits });
+        const totalIssues = issuesResponse.data.length;
+        const openIssues = issuesResponse.data.filter(issue => issue.state === 'opened').length;
+        const closedIssues = issuesResponse.data.filter(issue => issue.state === 'closed').length;
+        const totalBranches = branches.length;
+        const openMR = openMRs.data.length;
+        const closedMR = closedMRs.data.length;
+        const mergedMR = mergedMRs.data.length;
+
+        res.render('project', { project, totalCommits, totalIssues, openIssues, closedIssues, totalBranches, openMR, closedMR, mergedMR });
     } catch (error) {
         console.error(error.message);
         res.send("Error fetching project details");
     }
 });
+
 
 
 const PORT = 3000;
